d0b129f185757202340b7775106dbb776f90dea5
capstone <zephtao@uw.edu>
2024-02-23 13:11:50 -0800

    starting preset pose node

---
+++ createmate/createmate/preset_pose_action_server.py
@@ -0,0 +1,8 @@
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+'''
+    Read from preset pose file and send stretch robot to pose
+'''
+class SendToPresetPose(Node):
