6981970daef728056c5b98ad8ef1f43296cf92b0
Zephyr Tao <79123951+zephtao@users.noreply.github.com>
2024-02-29 11:49:58 -0800

    Update draw_shapes.py

--- createmate/createmate/draw_shapes.py
+++ createmate/createmate/draw_shapes.py
@@ -1,167 +1,69 @@
-# Modified from https://gist.github.com/hello-binit/8c46cfb773a8a0255b4703a09808c26e
+'''
+This file is for drawing shapes (circle, triangle, rectangle) with the trajectory client so that
+the stretch driver can also run simultaneously with this script.
+'''
+
 import sys
 import time
 import numpy as np
 import stretch_body.robot
 import stretch_body.trajectories
 stretch_body.trajectories.WAYPOINT_ISCLOSE_ATOL = 0.1
+from rclpy.time import Time
+from rclpy.duration import Duration
+from rclpy.action import ActionClient
+from control_msgs.action import FollowJointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+from enum import Enum
+from sensor_msgs.msg import JointState
 
+'''
+  Keep track of node state. Serves as the node's state
+  machine. The node can only become unblocked when a trajectory completes.
+  The base will move to align with the marker first. Then the arm/gripper
+  will continue
+'''
+class State(Enum):
+    BLOCK = 1,      # awaiting trajectory completion
+    BASE_READY = 2, # ready to move the base
+    ARM_READY = 3   # ready to move the arm/gripper
 
-r = stretch_body.robot.Robot()
-r.arm.motor.disable_guarded_mode()
-r.lift.motor.disable_guarded_mode()
-r.startup()
-if not r.is_calibrated():
-    print('Home the robot')
-    sys.exit(1)
-arm_init = 0.1
-lift_init = 1.0
+def __init__(self):
+     # create action client to move joints with stretch core driver
+    self.trajectory_client = ActionClient(self, FollowJointTrajectory, '/stretch_controller/follow_joint_trajectory')
+    server_reached = self.trajectory_client.wait_for_server(timeout_sec=60.0)
+    if not server_reached:
+        self.get_logger().error('Unable to connect to arm action server. Timeout exceeded.')
+        sys.exit()
 
+    # joint names whose positions are included in the message to the action server:
+    self.joint_names = ['translate_mobile_base', 'joint_lift', 'wrist_extension','gripper_aperture', 'joint_wrist_pitch']
 
-def setup_robot():
-    r.arm.move_to(arm_init)
-    r.lift.move_to(lift_init)
-    r.push_command()
-    r.end_of_arm.move_to('wrist_yaw', 1.5707)
-    r.end_of_arm.move_to('stretch_gripper', 100)
-    time.sleep(5)
-    input('Press enter to close the gripper')
-    r.end_of_arm.move_to('stretch_gripper', -100)
-    r.base.left_wheel.gains['enable_vel_watchdog'] = 0
-    r.base.left_wheel._dirty_gains = 1
-    r.base.right_wheel.gains['enable_vel_watchdog'] = 0
-    r.base.right_wheel._dirty_gains = 1
-    r.push_command()
-    time.sleep(3)
-    input('Press enter to command base towards whiteboard at 1cm/sec. NOTE: base is blind, you should be ready to press enter again to stop the base')
-    r.base.set_velocity(0.01, 0.0)
-    r.push_command()
-    input('Press enter to stop the base')
-    r.base.enable_freewheel_mode()
-    r.push_command()
+    # timer for node to call motion playback
+    time_period = 1.0
+    self.timer = self.create_timer(time_period, self.playback_motions)
+    self.state = State.BASE_READY
 
-def draw_circle_position_mode(n, diameter_m=0.2):
-    t = np.linspace(0, 2*np.pi, n, endpoint=True)
-    x = (diameter_m / 2) * np.cos(t) + arm_init
-    y = (diameter_m / 2) * np.sin(t) + lift_init
-    circle_mat = np.c_[x, y]
-    for pt in circle_mat:
-        r.arm.move_to(pt[0])
-        r.lift.move_to(pt[1])
-        r.push_command()
-        time.sleep(1.5)
+# CALLBACKS FOR EXTERNAL NODE MESSAGES
+def joint_states_callback(self, joint_states):
+    '''
+        callback to update current joint states from service
+    '''
+    self.joint_states = joint_states
 
-# def draw_circle_trajectory_mode(n, diameter_m=0.2, time_dt=1.5, globalv_m=None, globala_m=None):
-#     t = np.linspace(0, 2*np.pi, n, endpoint=True)
-#     x = (diameter_m / 2) * np.cos(t) + arm_init
-#     y = (diameter_m / 2) * np.sin(t) + lift_init
-#     circle_mat = np.c_[x, y]
-#     for i in range(n):
-#         pt = circle_mat[i]
-#         pt_t = i * time_dt
-#         r.arm.trajectory.add(t_s=pt_t, x_m=pt[0], v_m=globalv_m, a_m=globala_m)
-#         r.lift.trajectory.add(t_s=pt_t, x_m=pt[1], v_m=globalv_m, a_m=globala_m)
-#     r.follow_trajectory()
-#     time.sleep(n * time_dt + 0.5)
-        
-def draw_circle_trajectory(n, diameter_m=0.2):
-    t = np.linspace(0, 2*np.pi, n, endpoint=True)
-    x = (diameter_m / 2) * np.cos(t) + arm_init
-    y = (diameter_m / 2) * np.sin(t) + lift_init
-    circle_mat = np.c_[x, y]
-    time_dt = 15 / n
-    for i in range(n):
-        pt = circle_mat[i]
-        pt_t = i * time_dt
-        r.arm.trajectory.add(t_s=pt_t, x_m=pt[0])
-        r.lift.trajectory.add(t_s=pt_t, x_m=pt[1])
-    r.follow_trajectory()
-    time.sleep(n * time_dt + 0.5)
+def trajectory_server_response(self, future):
+    '''
+        response from trajectory server (not complete yet, but accepted or denied)
+    '''
+    goal_resp = future.result()
+    self.get_traj_server_res = goal_resp.get_result_async()
+    # add another callback awaiting final trajectory result
+    # TODO: add check for whether the trajectory was accepted
+    self.get_traj_server_res.add_done_callback(self.traj_res_callback)
 
-
-
-# NEW CODE DOWN HERE!!!!
-
-# Run draw_triangle_trajectory_mode(0.5, time_dt=1.0) 
-# to draw a triangle with 0.5m sides (each side taking 1 second to traverse)
-def draw_triangle_trajectory_mode(side_length_m, time_dt=1.5, globalv_m=None, globala_m=None):
-    # Calculate height of equilateral triangle
-    triangle_height = (side_length_m ** 2 - (side_length_m / 2) ** 2) ** 0.5
-
-    # Define the triangle's corner points relative to the initial arm and lift positions
-    triangle_corners = np.array([
-        [arm_init, lift_init],                                          # Starting corner (bottom left)
-        [arm_init + side_length_m, lift_init],                          # Move right (straight line now)
-        [arm_init + side_length_m / 2, lift_init + triangle_height],    # Move to top corner (> shape)
-        [arm_init, lift_init]                                           # Return to start (triangle shape)
-    ])
-    
-    # Calculate the time to reach each corner
-    # (assume equal time intervals between corners)
-    corner_times = np.arange(len(triangle_corners)) * time_dt
-    
-    # Clear any old trajectory data
-    # Not sure if needed but will leave it here
-    r.arm.trajectory.clear()
-    r.lift.trajectory.clear()
-    
-    # Following example from above 😅
-    for i, (corner, t_s) in enumerate(zip(triangle_corners, corner_times)):
-        r.arm.trajectory.add(t_s=t_s, x_m=corner[0], v_m=globalv_m, a_m=globala_m)
-        r.lift.trajectory.add(t_s=t_s, x_m=corner[1], v_m=globalv_m, a_m=globala_m)
-    
-    r.follow_trajectory()
-    time.sleep(len(triangle_corners) * time_dt + 0.5)
-
-def draw_triangle_position_mode(side_length_m):
-    # Calculate height of equilateral triangle
-    triangle_height = (side_length_m ** 2 - (side_length_m / 2) ** 2) ** 0.5
-
-    # Get (x1, y1) (x2, y2) (x3, y3) to represent a triangle
-    # Define the triangle's corner points relative to the initial arm and lift positions
-    triangle_corners = np.array([
-        [arm_init + side_length_m, lift_init],                          # Move right (straight line now)
-        [arm_init + side_length_m / 2, lift_init + triangle_height],    # Move to top corner (> shape)
-        [arm_init, lift_init]                                           # Return to start (triangle shape)
-    ])
-    
-    # Following example from above 😅
-    for x, y in triangle_corners:
-        r.arm.move_to(x)
-        r.push_command()
-        r.lift.move_to(y)
-        r.push_command()
-        time.sleep(1.5)
-
-# Run draw_square_trajectory_mode(0.5, time_dt=1.0) 
-# to draw a square with 0.5m sides (each side taking 1 second to traverse)
-def draw_square_trajectory_mode(n, side_length_m, time_dt=1.5, globalv_m=None, globala_m=None):
-    waypoints = [
-        (arm_init, lift_init),                                  # Start at bottom left
-        (arm_init + side_length_m, lift_init),                  # Move right (_)
-        (arm_init + side_length_m, lift_init + side_length_m),  # Move up (_|)
-        (arm_init, lift_init + side_length_m),                  # Move left (])
-        (arm_init, lift_init)                                   # Return to start (⬜️)
-    ]
-
-    # ???
-    r.arm.trajectory.clear()
-    r.lift.trajectory.clear()
-
-    for i, (x, y) in enumerate(waypoints):
-        pt_t = i * time_dt
-        r.arm.trajectory.add(t_s=pt_t, x_m=x, v_m=globalv_m, a_m=globala_m)
-        r.lift.trajectory.add(t_s=pt_t, x_m=y, v_m=globalv_m, a_m=globala_m)
-    r.follow_trajectory()
-    time.sleep(n * time_dt + 0.5)
-
-
-if __name__ == "__main__":
-    yn = input('setup robot? (y/[n]): ')
-    if yn == 'y':
-        setup_robot()
-    # draw_circle_position_mode(10, 1.5)
-    # draw_circle_trajectory(50)
-    # draw_triangle_trajectory_mode(0.5)
-    draw_triangle_position_mode(2)
-    # draw_square_trajectory_mode(0.5, 0.5, time_dt=1.0)
+def traj_res_callback(self, future):
+    '''
+    completion of trajectory request
+    '''
+    res = future.result().result
+    self.get_logger().info(f'trajectory complete, result: {res}')
