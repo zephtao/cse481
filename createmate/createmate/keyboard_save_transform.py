b0bc78bba02d6a9b55c8f1bd317f48765c1c6279
steph-osoriot <stephoso@uw.edu>
2024-03-08 00:33:41 -0800

    update saving poses process

--- createmate/createmate/keyboard_save_transform.py
+++ createmate/createmate/keyboard_save_transform.py
@@ -1,14 +1,14 @@
-from pynput import keyboard
-
 import rclpy
+
+from pynput import keyboard
 from rclpy.node import Node
-from std_msgs.msg import String
+from createmate_interfaces.msg import RecordPose
 
 # Node to listen for appropriate keyboard input and save frame transform accordingly
 class KeyboardInput(Node):
     def __init__(self):
         super().__init__('keyboard_input')
-        self.publisher = self.create_publisher(String, 'frame_transform_req', 10) # demo_commands topic with queue size of 10
+        self.publisher = self.create_publisher(RecordPose, 'pose_record_req', 10)
 
     def publish_message(self, data):
         msg = String()
@@ -17,13 +17,25 @@ class KeyboardInput(Node):
 
     def on_keyboard_press(self, key):
         if key == keyboard.Key.esc:
-            self.publish_message('quit')
+            msg = RecordPose()
+            msg.command = 'quit'
+            self.publish_message(msg)
             self.get_logger().info('quit message sent...')
             self.destroy_node()
             rclpy.shutdown()
-        if key == keyboard.Key.space:
 
-            self.publish_message('record_pose')
+        elif key == keyboard.Key.space:
+            msg = RecordPose()
+            msg.command = 'record_pose'
+            msg.name = input('type pose name:')
+            frame = input('Choose a frame\n1: map\n2: target_object1')
+
+            while frame not 1 or frame not 1:
+                frame=input('incorrect input, try again: ')
+
+            msg.frame = frame
+            self.publish_message(msg)
+        
             self.get_logger().info('record_pose message sent...')
 
     def get_keyboard_input(self):
@@ -37,9 +49,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# geometry_msgs.msg.TransformStamped(header=std_msgs.msg.Header(stamp=builtin_interfaces.msg.Time(sec=1708400927, nanosec=403441895), frame_id='link_grasp_center'), child_frame_id='target_object1', transform=geometry_msgs.msg.Transform(translation=geometry_msgs.msg.Vector3(x=0.5372364620953266, y=-0.22509781296434073, z=0.24617252052644534), rotation=geometry_msgs.msg.Quaternion(x=0.04452516070929938, y=0.03900741571137317, z=-0.2495200768241972, w=0.9665586701514699)))
-# geometry_msgs.msg.TransformStamped(header=std_msgs.msg.Header(stamp=builtin_interfaces.msg.Time(sec=1708400940, nanosec=681592773), frame_id='link_grasp_center'), child_frame_id='target_object1', transform=geometry_msgs.msg.Transform(translation=geometry_msgs.msg.Vector3(x=0.31434450884893206, y=-0.014661480732279075, z=0.3403597768549247), rotation=geometry_msgs.msg.Quaternion(x=0.7130356974202358, y=0.003661715498180148, z=-0.6911579283474606, w=-0.11775994279250879)))
-# geometry_msgs.msg.TransformStamped(header=std_msgs.msg.Header(stamp=builtin_interfaces.msg.Time(sec=1708400956, nanosec=90192627), frame_id='link_grasp_center'), child_frame_id='target_object1', transform=geometry_msgs.msg.Transform(translation=geometry_msgs.msg.Vector3(x=0.3134130916611446, y=-0.0146675722974699, z=0.34128054052590384), rotation=geometry_msgs.msg.Quaternion(x=0.7096565635922275, y=0.009792230278915477, z=-0.6951402156878204, w=-0.114331773843172)))
-# geometry_msgs.msg.TransformStamped(header=std_msgs.msg.Header(stamp=builtin_interfaces.msg.Time(sec=1708400993, nanosec=850075684), frame_id='link_grasp_center'), child_frame_id='target_object1', transform=geometry_msgs.msg.Transform(translation=geometry_msgs.msg.Vector3(x=0.2865763842636059, y=-0.015695035822294368, z=0.3422455873660746), rotation=geometry_msgs.msg.Quaternion(x=0.7048917279966858, y=0.03206055162495163, z=-0.702538481526339, w=-0.0924091705731414)))
-# geometry_msgs.msg.TransformStamped(header=std_msgs.msg.Header(stamp=builtin_interfaces.msg.Time(sec=1708401010, nanosec=928606689), frame_id='link_grasp_center'), child_frame_id='target_object1', transform=geometry_msgs.msg.Transform(translation=geometry_msgs.msg.Vector3(x=0.2879905802106184, y=-0.019189334169116223, z=0.4410683153758024), rotation=geometry_msgs.msg.Quaternion(x=-0.6974975078027082, y=-0.033772470337488286, z=0.7105814696353129, w=0.08620105490789105)))
---
+++ createmate/createmate/pose_listener.py
@@ -0,0 +1,116 @@
+import json
+import os
+import rclpy
+
+from createmate_interfaces.msg import RecordPose
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
+from rclpy.node import Node
+from rclpy.time import Time
+from std_msgs.msg import String, Header
+from tf2_ros import TransformStamped
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+class PoseListener(Node):
+
+  def __init__(self):
+    super().__init__('pose_listener')
+
+    # to listen to aruco-robot transforms
+    self.tf_buffer = Buffer()
+    self.tf_listener = TransformListener(self.tf_buffer, self)
+
+    #listen for keyboard requests to save poses
+    self.pose_subscriber = self.create_subscription(RecordPose, 'pose_record_req', self.record_pose, 10)
+
+    # subscriber for current location and pose rel to map
+    self.map_pose_sub = self.create_subscription(PoseWithCovarianceStamped, 'amcl_pose', self.save_map_pose, 1)
+    self.map_pose = None
+
+    # check if pose file already exists and load previous data
+    self.pose_filepath = '/home/hello-robot/cse481/zephyr_ws/save_poses.json'
+    if os.path.isfile(self.pose_filepath):
+      with open(self.pose_filepath, 'r') as pose_file:
+        self.poses = json.load(pose_file)
+    else:
+      self.poses = {}
+
+  def save_map_pose(self, msg):
+    '''
+      save pose of robot on map
+      msg: Geometry_msgs/msg/PoseWithCovarianceStamped
+    '''
+    self.map_pose = PoseStamped()
+    self.map_pose.header = Header()
+    self.map_pose.header.frame_id = msg.header.frame_id
+    # posewithcovariancestamped -> posewithcovariance -> pose
+    self.map_pose.pose = msg.pose.pose
+      
+  def record_pose(self, rec_msg):
+    '''
+      callback dealing with keyboard request to save pose
+      rec_msg: createmate_interfaces/msg/RecordPose
+    '''
+    if rec_msg.command == 'record_pose':
+      self.get_logger().info(f'Received request to record pose in {rec_msg.frame} frame')
+
+      if rec_msg.frame == 'map': # position on map
+        if self.map_pose is not None:
+          position = self.map_pose.pose.position
+          orientation = self.map_pose.pose.orientation
+          vector = {'x': position.x, 'y': position.y, 'z': position.z}
+          quaternion = {'x': orientation.x, 'y': orientation.y, 'z': orientation.z, 'w': orientation.w}
+          pose_entry =  {'type': 'pose', 'frame_id': self.map_pose.header.frame_id, 'child_frame_id': '', 'vector': vector, 'quaternion': quaternion}
+        else:
+          self.get_logger().info('could not localize robot on map')
+          return
+      else : # an aruco frame
+        tf = get_gripper_aruco_tf(rec_msg.frame)
+        translation = tf.transform.translation
+        rotation = tf.transform.rotation
+        tf_vector = {'x': translation.x, 'y': translation.y, 'z': translation.z}
+        tf_quaternion = {'x': rotation.x, 'y': rotation.y, 'z': rotation.z, 'w': rotation.w}
+        pose_entry = {'type': 'transform', 'frame_id': rec_msg.frame, 'child_frame_id': 'link_grasp_center', 'vector': tf_vector, 'quaternion': tf_quaternion}
+
+      # add to pose dict
+      self.poses[rec_msg.name] = pose_entry
+
+    elif rec_msg.command == 'quit':
+      self.get_logger().info(f'Got quit message... writing all requested transforms to {self.pose_filepath}')
+
+      # write all poses to filepath
+      json_obj = json.dumps(self.poses, indent=2)
+      with open(self.pose_filepath, 'w') as pose_file:
+        pose_file.write(json_obj)
+      
+      # shutdown
+      self.get_logger().info('shutting down')
+      self.destroy_node()
+      rclpy.shutdown()
+
+  def get_gripper_aruco_tf(self, target_frame):
+    '''
+      Retrieves tf from
+    '''
+    from_frame_rel = 'link_grasp_center'
+    try:
+      now = Time()
+      trans = self.tf_buffer.lookup_transform(
+        target_frame,
+        from_frame_rel,
+        now)
+      return trans
+    except TransformException as ex:
+      self.get_logger().info(f'Could not transform {from_frame_rel} to {target_frame}: {ex}')
+
+def main():
+  rclpy.init()
+  node = PoseListener()
+  try:
+    rclpy.spin(node)
+  except KeyboardInterrupt:
+    pass
+
+if __name__ == '__main__':
+    main()
